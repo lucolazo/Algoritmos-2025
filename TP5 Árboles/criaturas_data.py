62167cf252a2f664f692edb4854ddf79c49744cb
Lucolazo <anaacolazo@gmail.com>
2025-10-05 18:57:38 -0300

    Clases y listas usadas

---
+++ TP5 Árboles/criaturas_data.py
@@ -0,0 +1,192 @@
+criaturas = [
+    {   'Nombre': 'Ceto',
+        'Derrotada_por': None, 
+        'Descripcion': 'Deidad marina primordial',
+        'Capturada_por': 'Perseo'
+    },
+    {   'Nombre': 'Tifon',
+        'Derrotada_por': 'Zeus', 
+        'Descripcion': 'Monstruo gigante',
+        'Capturada_por': 'Zeus'
+    },
+    {   'Nombre': 'Equidna',
+        'Derrotada_por': 'Argos Panoptes', 
+        'Descripcion': 'Madre de todos los monstruos',
+        'Capturada_por': 'Argos Panoptes'
+    },
+    {   'Nombre': 'Dino',
+        'Derrotada_por': None, 
+        'Descripcion': 'Monstruo marino',
+        'Capturada_por': 'Perseo'
+    },
+    {   'Nombre': 'Pefredo',
+        'Derrotada_por': None, 
+        'Descripcion': 'Criatura mitológica',
+        'Capturada_por': 'Heracles'
+    },
+    {   'Nombre': 'Enio',
+        'Derrotada_por': None, 
+        'Descripcion': 'Diosa de la guerra',
+        'Capturada_por': 'Ares'
+    },
+    {   'Nombre': 'Escila',
+        'Derrotada_por': None, 
+        'Descripcion': 'Monstruo marino',
+        'Capturada_por': 'Odiseo'
+    },
+    {   'Nombre': 'Caribdis',
+        'Derrotada_por': None, 
+        'Descripcion': 'Monstruo marino',
+        'Capturada_por': 'Odiseo'
+    },
+    {   'Nombre': 'Euriale',
+        'Derrotada_por': None, 
+        'Descripcion': 'Gorgona',
+        'Capturada_por': 'Perseo'
+    },
+    {   'Nombre': 'Esteno',
+        'Derrotada_por': None,
+        'Descripcion': 'Gorgona',
+        'Capturada_por': 'Perseo'
+    },
+    {   'Nombre': 'Medusa',
+        'Derrotada_por': 'Perseo',
+        'Descripcion': 'Gorgona',
+        'Capturada_por': 'Perseo'
+    },
+    {   'Nombre': 'Ladón',
+        'Derrotada_por': 'Heracles', 
+        'Descripcion': 'Dragón de los Hesperides',
+        'Capturada_por': 'Heracles'
+    },
+    {   'Nombre': 'Hidra de Lerna',
+        'Derrotada_por': 'Heracles', 
+        'Descripcion': 'Serpiente de múltiples cabezas',
+        'Capturada_por': 'Heracles'
+    },
+    {   'Nombre': 'Cerbero',
+        'Derrotada_por': None, 
+        'Descripcion': 'Perro guardián del inframundo',
+        'Capturada_por': 'Heracles'
+    },
+    {   'Nombre': 'Jabalí de Erimanto',
+        'Derrotada_por': None, 
+        'Descripcion': 'Jabalí gigante',
+        'Capturada_por': 'Heracles'
+    },
+    {   'Nombre': 'León de Nemea',
+        'Derrotada_por': 'Heracles', 
+        'Descripcion': 'León invulnerable',
+        'Capturada_por': 'Heracles'
+    },
+    {   'Nombre': 'Aves del Estínfalo',
+        'Derrotada_por': None, 
+        'Descripcion': 'Aves carnívoras',
+        'Capturada_por': 'Heracles'
+    },
+    {   'Nombre': 'Basilisco',
+        'Derrotada_por': None, 
+        'Descripcion': 'Serpiente mitológica',
+        'Capturada_por': 'Cadmo'
+    },
+    {   'Nombre': 'Sirenas',
+        'Derrotada_por': None, 
+        'Descripcion': 'Criaturas marinas',
+        'Capturada_por': 'Orfeo'
+    },
+    {   'Nombre': 'Talos',
+        'Derrotada_por': 'Medea', 
+        'Descripcion': 'Autómata de bronce',
+        'Capturada_por': 'Medea'
+    },
+    {   'Nombre': 'Aguila del Cáucaso',
+        'Derrotada_por': None, 
+        'Descripcion': 'Ave gigante',
+        'Capturada_por': 'Heracles'
+    },
+    {   'Nombre': 'Cierva Cerinea',
+        'Derrotada_por': None, 
+        'Descripcion': 'Cierva sagrada',
+        'Capturada_por': 'Artémis'
+    },
+    {   'Nombre': 'Quimera',
+        'Derrotada_por': 'Belerofonte', 
+        'Descripcion': 'Monstruo con cabeza de león',
+        'Capturada_por': 'Belerofonte'
+    },
+    {   'Nombre': 'Esfinge',
+        'Derrotada_por': 'Edipo', 
+        'Descripcion': 'Criatura con cuerpo de león y cabeza de mujer',
+        'Capturada_por': 'Edipo'
+    },
+    {   'Nombre': 'Dragón de Cólquida',
+        'Derrotada_por': None, 
+        'Descripcion': 'Dragón que custodiaba el Vellocino de Oro',
+        'Capturada_por': 'Jasón'
+    },
+    {   'Nombre': 'Cerda de Cromión',
+        'Derrotada_por': 'Teseo', 
+        'Descripcion': 'Cerda gigante que aterrorizaba la región',
+        'Capturada_por': 'Teseo'
+    },
+    {   'Nombre': 'Ortro',
+        'Derrotada_por': 'Heracles', 
+        'Descripcion': 'Perro de dos cabezas',
+        'Capturada_por': 'Heracles'
+    },
+    {   'Nombre': 'Harpías',
+        'Derrotada_por': None, 
+        'Descripcion': 'Criaturas con cuerpo de ave y rostro de mujer',
+        'Capturada_por': 'Zetes y Calais'
+    },
+    {   'Nombre': 'Minotauro de Creta',
+        'Derrotada_por': 'Teseo', 
+        'Descripcion': 'Criatura con cuerpo de hombre y cabeza de toro',
+        'Capturada_por': 'Teseo'
+    },
+    {   'Nombre': 'Toro de Creta',
+        'Derrotada_por': 'Teseo', 
+        'Descripcion': 'Toro sagrado de Creta',
+        'Capturada_por': 'Teseo'
+    },
+    {   'Nombre': 'Jabalí de Calidón',
+        'Derrotada_por': 'Atalanta', 
+        'Descripcion': 'Jabalí gigante que aterrorizaba la región',
+        'Capturada_por': 'Atalanta'
+    },
+    {   'Nombre': 'Carcinos',
+        'Derrotada_por': None, 
+        'Descripcion': 'Cangrejo gigante',
+        'Capturada_por': 'Heracles'
+    },
+    {   'Nombre': 'Gerión',
+        'Derrotada_por': 'Heracles', 
+        'Descripcion': 'Gigante con tres cuerpos',
+        'Capturada_por': 'Heracles'
+    },
+    {   'Nombre': 'Cloto',
+        'Derrotada_por': None, 
+        'Descripcion': 'Hila el destino',
+        'Capturada_por': None
+    },
+    {   'Nombre': 'Láquesis',
+        'Derrotada_por': None, 
+        'Descripcion': 'Mide el destino',
+        'Capturada_por': None
+    },
+    {   'Nombre': 'Atropos',
+        'Derrotada_por': None, 
+        'Descripcion': 'Corta el destino',
+        'Capturada_por': None
+    },
+    {   'Nombre': 'Pitón',
+        'Derrotada_por': 'Apolo', 
+        'Descripcion': 'Serpiente monstruosa',
+        'Capturada_por': 'Apolo'
+    },
+    {   'Nombre': 'Argos Panoptes',
+        'Derrotada_por': 'Hermes', 
+        'Descripcion': 'Gigante de cien ojos',
+        'Capturada_por': 'Hermes'
+    },
+]
