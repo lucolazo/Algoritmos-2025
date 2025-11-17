eb012bcfd7ad6ce48390b4a0ed52b373c6798342
Lucolazo <anaacolazo@gmail.com>
2025-11-17 20:42:49 -0300

    listas usadas

---
+++ Parcial 2° cuatri/pokemon_list.py
@@ -0,0 +1,82 @@
+pokemon_data = [
+  {
+    "nombre": "Pikachu",
+    "numero": 25,
+    "tipo": ["Eléctrico"],
+    "debilidades": ["Tierra"],
+    "tiene_mega": False,
+    "tiene_gigamax": True
+  },
+  {
+    "nombre": "Charizard",
+    "numero": 6,
+    "tipo": ["Fuego", "Volador"],
+    "debilidades": ["Roca", "Agua", "Eléctrico"],
+    "tiene_mega": True,
+    "tiene_gigamax": True
+  },
+  {
+    "nombre": "Gyarados",
+    "numero": 130,
+    "tipo": ["Agua", "Volador"],
+    "debilidades": ["Eléctrico", "Roca", "Hada"],
+    "tiene_mega": True,
+    "tiene_gigamax": False
+  },
+  {
+    "nombre": "Gardevoir",
+    "numero": 282,
+    "tipo": ["Psíquico", "Hada"],
+    "debilidades": ["Veneno", "Fantasma", "Acero"],
+    "tiene_mega": True,
+    "tiene_gigamax": False
+  },
+  {
+    "nombre": "Lucario",
+    "numero": 448,
+    "tipo": ["Lucha", "Acero"],
+    "debilidades": ["Fuego", "Lucha", "Tierra"],
+    "tiene_mega": True,
+    "tiene_gigamax": False
+  },
+  {
+    "nombre": "Greninja",
+    "numero": 658,
+    "tipo": ["Agua", "Siniestro"],
+    "debilidades": ["Lucha", "Bicho", "Hada", "Planta", "Eléctrico"],
+    "tiene_mega": False,
+    "tiene_gigamax": False
+  },
+  {
+    "nombre": "Corviknight",
+    "numero": 823,
+    "tipo": ["Volador", "Acero"],
+    "debilidades": ["Fuego", "Eléctrico"],
+    "tiene_mega": False,
+    "tiene_gigamax": True
+  },
+  {
+    "nombre": "Cinderace",
+    "numero": 815,
+    "tipo": ["Fuego"],
+    "debilidades": ["Tierra", "Roca", "Agua"],
+    "tiene_mega": False,
+    "tiene_gigamax": True
+  },
+  {
+    "nombre": "Miraidon",
+    "numero": 1008,
+    "tipo": ["Eléctrico", "Dragón"],
+    "debilidades": ["Hielo", "Hada", "Tierra", "Dragón"],
+    "tiene_mega": False,
+    "tiene_gigamax": False
+  },
+  {
+    "nombre": "Iron Valiant",
+    "numero": 996,
+    "tipo": ["Hada", "Lucha"],
+    "debilidades": ["Veneno", "Volador", "Psíquico", "Hada"],
+    "tiene_mega": False,
+    "tiene_gigamax": False
+  }
+]
---
+++ Parcial 2° cuatri/starwars_relations.py
@@ -0,0 +1,15 @@
+relations = [
+    ('C-3PO','R2-D2',9),
+    ('Luke Skywalker','Leia',6),
+    ('Luke Skywalker','Darth Vader',4),
+    ('Luke Skywalker','R2-D2',6),
+    ('C-3PO','Luke Skywalker',6),
+    ('Han Solo','Chewbacca',7),
+    ('Han Solo','Luke Skywalker',4),
+    ('Leia','Chewbacca',5),
+    ('Rey','BB-8',3),
+    ('Rey','Kylo Ren',2),
+    ('Darth Vader','Leia',2),
+    ('Boba Fett','Han Solo',1),
+    ('Kylo Ren','Darth Vader',1)
+]
